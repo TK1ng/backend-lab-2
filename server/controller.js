c3b6cac891629c219d253a8e2ea5fcff6bfa8c14
Taryn King <marshaeking@gmail.com>
2022-11-29 15:42:03 -0600

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,49 @@
+const houses = require('./db.json');
+
+let globalHouseId = 4;
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses);
+    },
+    deleteHouses: (req, res) => {
+        const { id } = req.params;
+
+        const houseIndex = houses.findIndex(house => house.id === id);
+
+        houses.splice(houseIndex, 1);
+        res.status(200).send(houses);
+    },
+    createHouses: (req, res) => {
+        const { address, price, imageURL } = req.body;
+
+        let newHouse = {
+            id: globalHouseId,
+            address,
+            price: +price,
+            imageURL
+        }
+
+        houses.push(newHouse);
+        globalHouseId++
+
+        res.status(200).send(houses);
+    },
+    updateHouses: (req, res) => {
+        const { id } = req.params;
+        const { type } = req.body;
+
+        const house = houses.find(house => house.id === +id);
+
+        if (type === 'minus' && house.price === 0) {
+            res.status(400).send('Price cannot be less than zero!')
+        } else if (type === 'minus') {
+            house.price -= 10000;
+            res.status(200).send(houses);
+        } else if (type === 'plus') {
+            house.price += 10000;
+            res.status(200).send(houses);
+        }
+    },
+
+}
